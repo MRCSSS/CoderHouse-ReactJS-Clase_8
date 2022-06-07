dfd3d6f49783068af0d5cc68bdf83c237e69a152
Marcos Solis S <mss@192.168.1.10>
2022-06-07 01:56:01 -0500

    Last update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import ItemCount from './components/ItemCount';
 
 function App() {
+
+  const onAdd = (count) => {
+    alert(`Son ${count} productos`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ItemCount inicial={1} max={15} onAdd={onAdd}/>
+    </>
   );
 }
 
